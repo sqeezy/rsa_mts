e93a60721801d70ae805ca450332d5595fec7221
sqeezy <sqeezy@gmx.de>
2014-06-11 20:03:01 +0200

    Make FileRead class static.

--- rsa_mts/FileRead.cs
+++ rsa_mts/FileRead.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace rsa_mts
 {
-    class FileRead
+    public static class FileRead
     {
-        public FileRead()
+        public static byte[] Read(string filepath)
         {
-            
+            if (!File.Exists(filepath))
+            {
+                throw new ArgumentException("No file with this path.");
+            }
+            else
+            {
+                try
+                {
+                    return File.ReadAllBytes(filepath);
+                }
+                catch (IOException)
+                {
+                    throw new Exception("Could not read this file.");
+                }
+            }
         }
     }
 }
