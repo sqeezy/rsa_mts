da5c4face02e1863a4a572d0a1895e11cbc77614
Anton Herzog <sqeezy@gmx.de>
2014-06-13 16:07:02 +0200

    Comments for RSA.cs. Extended RsaException. Lorem ipsum style text in Testfile.txt

--- rsa_mts/RSA.cs
+++ rsa_mts/RSA.cs
@@ -5,14 +5,25 @@ using System.Numerics;
 
 namespace rsa_mts
 {
+    /// <summary>
+    /// A class to encrypt single chunks of data using the RSA encryption.
+    /// This is not the standard implementation which uses padding and encrypt greater blocks of data.
+    /// </summary>
     internal class RSA
     {
         private BigInteger _d;
         private BigInteger _e;
         private BigInteger _n;
 
-        public RSA(int primeOne = 1327,
-                   int primeTwo = 2099)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RSA"/> class.
+        /// This constructor is private because a method to find e and d for the algorithm is missing at the momment.
+        /// </summary>
+        /// <param name="primeOne">The prime one.</param>
+        /// <param name="primeTwo">The prime two.</param>
+        /// <exception cref="System.ArgumentException">Inputs have to be prime.</exception>
+        private RSA(int primeOne,
+                   int primeTwo)
         {
             if (!IsPrime(primeOne) || !IsPrime(primeTwo))
             {
@@ -24,26 +35,58 @@ namespace rsa_mts
             _n = BigInteger.Multiply(p, q);
             var phiN = new BigInteger((primeOne - 1)*(primeTwo - 1));
 
-            _e = new BigInteger(65537); //Fermatzahl
+            _e = new BigInteger(65537); //Fermatzahl - Default that works with given default primes.
             _d = ModInverse(_e, phiN);
         }
 
-        public int Encrypt(int msg)
+        /// <summary>
+        /// Constructor with default values.
+        /// </summary>
+        public RSA() : this(1327, 2099)
         {
-            return (int) ModPow(new BigInteger(msg), _e, _n);
-        }
-
-        public int Decrypt(int msg)
-        {
-            return (int) ModPow(new BigInteger(msg), _d, _n);
         }
 
         /// <summary>
-        /// Sieb des Eratosthenes
+        /// Encrypts the specified message.
         /// </summary>
-        /// <param name="n"></param>
-        /// <returns>Arraylist mit Booleanwerten danach wird in isPrimzahl geschaut ob 
-        /// diese Zahl in der Liste ist</returns>
+        /// <param name="msg">The message.</param>
+        /// <returns></returns>
+        /// <exception cref="RsaException">A problem occured while encrypting.</exception>
+        public int Encrypt(int msg)
+        {
+            try
+            {
+                return (int) ModPow(new BigInteger(msg), _e, _n);
+            }
+            catch (Exception e)
+            {
+                throw new RsaException("A problem occured while encrypting: ", e);
+            }
+        }
+
+        /// <summary>
+        /// Decrypts the specified message.
+        /// </summary>
+        /// <param name="msg">The message.</param>
+        /// <returns></returns>
+        /// <exception cref="RsaException">A problem occured while decrypting.</exception>
+        public int Decrypt(int msg)
+        {
+            try
+            {
+                return (int) ModPow(new BigInteger(msg), _d, _n);
+            }
+            catch (Exception e)
+            {
+                throw new RsaException("A problem occured while decrypting: ", e);
+            }
+        }
+
+        /// <summary>
+        /// Sieve of Erathosthenes
+        /// Method to determine weather a number is prime or not.
+        /// </summary>
+        /// <param name="n">The number which we want to check.</param>
         private bool IsPrime(int n)
         {
             // Legt eine neue Liste an
@@ -78,11 +121,11 @@ namespace rsa_mts
         }
 
         /// <summary>
-        /// Methode um Modualre Inverse mittels Big Integer zu berechen
+        /// Computes the modular inverse of a number in relation to a certain modulus.
         /// </summary>
-        /// <param name="a"></param>
-        /// <param name="n"></param>
-        /// <returns> modulare Inverse zweier Zahlen</returns>
+        /// <param name="a">The number to compute the inverse to.</param>
+        /// <param name="n">The modulus to which we relate</param>
+        /// <returns>The modular inverse.</returns>
         private BigInteger ModInverse(BigInteger a,
                                       BigInteger n)
         {
@@ -107,19 +150,26 @@ namespace rsa_mts
             return v;
         }
 
-        private BigInteger ModPow(BigInteger basis,
-                                  BigInteger exp,
-                                  BigInteger mod)
+        /// <summary>
+        /// Computes the modulus of a number which we get from raising some base to a certain power.
+        /// </summary>
+        /// <param name="baseToRaise">The basis.</param>
+        /// <param name="power">The power.</param>
+        /// <param name="modulus">The modulus.</param>
+        /// <returns></returns>
+        private BigInteger ModPow(BigInteger baseToRaise,
+                                  BigInteger power,
+                                  BigInteger modulus)
         {
             BigInteger result = 1;
-            while (exp > 0)
+            while (power > 0)
             {
-                if (!exp.IsEven)
+                if (!power.IsEven)
                 {
-                    result = (result*basis)%mod;
+                    result = (result*baseToRaise)%modulus;
                 }
-                exp >>= 1;
-                basis = (basis*basis)%mod;
+                power >>= 1;
+                baseToRaise = (baseToRaise*baseToRaise)%modulus;
             }
             return result;
         }
--- rsa_mts/RsaException.cs
+++ rsa_mts/RsaException.cs
@@ -2,9 +2,17 @@
 
 namespace rsa_mts
 {
+    /// <summary>
+    /// A exception class special for this rsa implementation.
+    /// </summary>
     internal class RsaException : Exception
     {
         public RsaException(string msg):base(msg)
+        {
+        }
+
+        public RsaException(string msg,
+                            Exception e) : base(msg, e)
         {
         }
     }
